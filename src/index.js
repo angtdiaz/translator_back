f4aee74e4aec7a333ca6e620b538a1b35b2ee8b4
Angie Tuarez <nicoltuarezd@gmail.com>
2023-01-16 07:05:25 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -105,5 +105,5 @@ app.post("/traslation", async (req, res) => {
   }
 });
 
-app.listen(3000);
-console.log("Server listening on port 3000");
+app.listen(80);
+console.log("Server listening on port 80");
